67d55ee32fea3b4e5128182f9e3d10c6e27531b4
LuisFOsG <luisfosgdev@gmail.com>
2022-04-24 01:02:34 -0500

    feat: filter input

--- src/componets/Loading.jsx
+++ src/componets/Loading.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 import LoadingSpin from "react-loading-spin";
 
 function Loading() {
-  return (
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [])
+
+  if (isLoading) return (
     <LoadingSpin
       primaryColor={'#2c2d05'}
     />
   )
+
+  return null;
 }
 
 export default Loading
--- src/componets/exercises/Exercise2.jsx
+++ src/componets/exercises/Exercise2.jsx
@@ -4,6 +4,19 @@ const Exercise = () => {
   useChoiceInput('#choices-exercise2', {
     removeItems: true,
     removeItemButton: true,
+    maxItemCount: 3,
+    placeholderValue: "Ingresa 3 numeros",
+    loadingText: 'Cargando...',
+    customAddItemText: 'Solo puede ingresar numeros',
+    addItemText: (value) => {
+      return `Presiona Enter para agregar <b>"${value}"</b>`;
+    },
+    maxItemText: (maxItemCount) => {
+      return `Solo puedes agregar ${maxItemCount} valores`;
+    },
+    addItemFilter: (value) => {
+      return /^[0-9]*(\.?)[0-9]+$/.test(value)
+    }
   });
 
   return (
